e0b08ff4a56385bbab6f223e28aa7c52f04c3053
StewieC <129508680+StewieC@users.noreply.github.com>
2023-12-01 14:04:48 +0300

    Add files via upload

---
+++ Constructors/src/thiscontsuctors/Thiss.java
@@ -0,0 +1,20 @@
+package thiscontsuctors;
+
+public class Thiss {
+	public static void main(String[]args) {
+		
+		Student koshy = new Student();
+		
+	}
+	
+
+}
+class Student{
+	int reg;
+	int marks;
+	
+	Student(){
+		this.reg = 0;
+		this.marks = 0;
+	}
+}
