a7d2102613925f255caafd128dc7d68f8f1932a9
StewieC <129508680+StewieC@users.noreply.github.com>
2024-03-08 18:44:09 +0300

    Add files via upload

---
+++ Objects/src/objects1/Methods.java
@@ -0,0 +1,15 @@
+package objects1;
+
+public class Methods {
+	public static void main(String[]args) {
+		System.out.print("sum is: "+summation(10,22));
+	}
+	
+	//creating a method
+	public static int summation(int x, int y) {
+		
+		int sum = x + y;
+		return sum;
+	}
+
+}
---
+++ Objects/src/objects1/example1.java
@@ -0,0 +1,13 @@
+package objects1;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//there are different types of objects
+		//where 
+
+	}
+
+}
